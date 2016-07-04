590c63aaef70d63396b2926718946e428d281f3d
gustavoelizalde <gustavoelizalde@gmail.com>
2016-07-04 19:46:28 -0400

    Hacer el método en el controller para procesar el formulario y guardar los datos en el modelo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
   def x
+  	@nombre = params[:nombre]
+  	@email = params[:email]
+  	@edad = params[:edad].to_i
+
+  	@all_users = User.all()
+
+  	if @nombre.present?
+  		user = User.new(nombre: @nombre, email: @email, edad: @edad)
+
+	  	if user.save
+	  		redirect_to pages_x_path,
+	  			notice: "El Usuario ha sido guardado con éxito"
+	  	else
+	  		redirect_to pages_x_path,
+	  			notice: "El Usuario No ha sido guardado"
+	  	end
+	end
+
   end
 end
