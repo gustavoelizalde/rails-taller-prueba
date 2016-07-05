7e2fe4bf42a136dbf5ff910901ba4a8c3728aff5
gustavoelizalde <gustavoelizalde@gmail.com>
2016-07-04 21:21:40 -0400

    adding jquery.easing.min

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require jquery.easing.min
 //= require scrollreveal
 //= require jquery.magnific-popup
 //= require creative
